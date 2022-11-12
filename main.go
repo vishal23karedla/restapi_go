28f77092adc704d64beb59b1679ced233b0c758f
vishal23karedla <vishal23karedla@gmail.com>
2022-11-13 00:32:16 +0530

    basic rest endpoints created

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println("Main funcion is running")
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", getHome).Methods("GET")
+	r.HandleFunc("/", postHome).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":4000", r))
+}
+
+func postHome(response http.ResponseWriter, request *http.Request) {
+	//Handle the JSON object
+
+	// fmt.Println(request.Body)
+
+	response.Write([]byte("This is the post route"))
+}
+
+func getHome(response http.ResponseWriter, request *http.Request) {
+	response.Write([]byte("Hey there! Welcome Home"))
 }
